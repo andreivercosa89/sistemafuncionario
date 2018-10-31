7ca2419a6833e193e4e552cfc6b39feffd32ae53
andreivercosa <aluno@MacBook-Pro-de-Aluno.local>
2018-10-30 21:07:53 -0400

    sistemafuncionario

---
+++ sistemafuncionario/Funcionario.cs
@@ -0,0 +1,25 @@
+﻿using System;
+namespace sistemafuncionario{
+    public class Funcionario
+    {
+        public string Nome{
+            set;get;
+        }
+
+        public double Salario{
+            get;set;
+                
+        }
+
+        public double novoSalario(){
+            if(Salario <= 1000){
+                return Salario * 1.3;
+            }else if(Salario <=1500){
+                return Salario * 1.2;
+            }
+            else{
+                return Salario * 1.1;
+            }
+        }
+    }
+}
---
+++ sistemafuncionario/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace sistemafuncionario
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int resp=1;
+            do
+            {
+
+                Funcionario funcionario = new Funcionario();
+
+                Console.WriteLine("\nInforme o nome do funcionario: ");
+                funcionario.Nome = Console.ReadLine();
+
+                Console.WriteLine("\nInforme o salario: ");
+                funcionario.Salario = Double.Parse(Console.ReadLine());
+
+
+                Console.WriteLine("\nO Novo salario do Funcionario {0} e {1}.", funcionario.Nome, funcionario.novoSalario());
+                Console.WriteLine("\nDeseja Continuar \n1 - Sim\n2 - nao");
+                resp = Int32.Parse(Console.ReadLine());
+            } while (resp == 1);
+
+        }
+    }
+}
